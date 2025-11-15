342f93e8ded2388743e2bc9cf45ff5b5c52b909e
saad995571 <saadkhanft88@gmail.com>
2025-11-15 11:34:29 +0530

    Update number.py

--- Project/number.py
+++ Project/number.py
@@ -1,11 +1,27 @@
-import random
-random_number=random.randint(1,100)
+     import random
+random_number = random.randint(1,100)
+print("Welcome User!")
+
+attempts = 0 
+score = 100
+
 while True:
-    b=int(input("Guess the number:"))
-    if b==random_number:
-        print("Jacpot!")
-        break
-    elif b<random_number:
-        print("You are low")
-    elif b>random_number:
-        print("You are high")
+    a= int(input("Enter Your guessed Number: "))
+    attempts += 1
+
+    if a==random_number:
+       print("Congratulations!!, You guessed it right!!")
+       print(f"You guessed it in{attempts} attempts. ")
+       print(f"Your final score is {score}")
+       break
+    
+
+    elif a < random_number:
+        print("TOO LOW")
+        score -= 5
+        
+    elif a > random_number:
+        print("TOO HIGH!") 
+        score -= 5
+
+    print(f"current score: {score}")
